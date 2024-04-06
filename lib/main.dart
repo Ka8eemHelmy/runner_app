ce98a8e40b781a7f70fccb3c119e1bc026e9a4cb
Kareem <Ka8eemHelmy@gmail.com>
2024-04-06 15:15:42 +0200

    Edit in Main File and add Comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'my_app.dart';
-import 'view_model/cubits/observer.dart';
 
 void main() {
   // Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
+  /// Kareem Edit Here
 }
